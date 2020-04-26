ce85428bb677aa192ee1fc9060e1917324b89817
estellar_boyz <tayalone@gmail.com>
2020-04-26 08:48:32 +0700

    :white_check_mark: [ADD] add test middlewares

---
+++ middlewares/generateQueryParam/index.test.js
@@ -0,0 +1,47 @@
+const httpMocks = require('node-mocks-http')
+
+const generateQueryParam = require('./index')
+
+describe(`Test middlewares/generateQueryParam`, () => {
+  describe('Case: Bad Parameter', () => {
+    let next = null
+    let response = null
+
+    beforeAll(async (done) => {
+      const req = null
+      const res = httpMocks.createResponse()
+      next = jest.fn()
+      response = await generateQueryParam(req, res, next)
+      done()
+    })
+    test(`status must be 400`, async () => {
+      expect(response.statusCode).toEqual(400)
+    })
+    test(`'error from middleware/generateQueryParam'`, async () => {
+      expect(response._getData().errMessage).toEqual(
+        'error from middleware/generateQueryParam'
+      )
+    })
+    test('error_from_middleware/generateQueryParam'.toUpperCase(), async () => {
+      expect(response._getData().errKey).toEqual(
+        'error_from_middleware/generateQueryParam'.toUpperCase()
+      )
+    })
+    test(`next must be not called`, async () => {
+      expect(next).not.toBeCalled()
+    })
+  })
+  describe('Case: Success', () => {
+    let next = null
+    beforeAll(async (done) => {
+      const req = { query: '' }
+      const res = httpMocks.createResponse()
+      next = jest.fn()
+      response = await generateQueryParam(req, res, next)
+      done()
+    })
+    test(`next must be called`, async () => {
+      expect(next).toBeCalled()
+    })
+  })
+})
