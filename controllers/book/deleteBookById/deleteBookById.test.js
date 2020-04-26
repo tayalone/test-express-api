cdab59f684069a81ba39104ffcf71fdca11839c8
estellar_boyz <tayalone@gmail.com>
2020-04-26 21:25:34 +0700

    :white_check_mark: [ADD] test deleteBookById

--- controllers/book/deleteBookById/deleteBookById.test.js
+++ controllers/book/deleteBookById/deleteBookById.test.js
@@ -1,6 +1,6 @@
 const httpMocks = require('node-mocks-http')
 const mongoose = require('mongoose')
-const getBookById = require('./index')
+const deleteBookById = require('./index')
 
 const clearModels = require('../../../mock/clearModels')
 const installBooks = require('../../../mock/installBook')
@@ -8,11 +8,11 @@ const installBooks = require('../../../mock/installBook')
 const { mongoTestUrl } = require('../../../variables')
 
 const Book = require('../../../models/books.model')
-const mockReq = (currentBook) => {
-  return { currentBook }
+const mockReq = (bookId) => {
+  return { params: { bookId } }
 }
 
-describe(`Test controllers/book/getBookById`, () => {
+describe(`Test controllers/book/deleteBookById`, () => {
   beforeAll(async (done) => {
     await mongoose.connect(mongoTestUrl, {
       useCreateIndex: true,
@@ -31,34 +31,41 @@ describe(`Test controllers/book/getBookById`, () => {
     beforeAll(async (done) => {
       const req = null
       const res = httpMocks.createResponse()
-      response = await getBookById(req, res)
+      response = await deleteBookById(req, res)
       done()
     })
     test(`status must be 400`, async () => {
       expect(response.statusCode).toEqual(400)
     })
-    test(`errMessage must be 'error from getBookById'`, async () => {
-      expect(response._getData().errMessage).toEqual('error from getBookById')
+    test(`errMessage must be 'error from deleteBookById'`, async () => {
+      expect(response._getData().errMessage).toEqual(
+        'error from deleteBookById'
+      )
     })
-    test(`errKey must be ${'error_from_getBookById'.toUpperCase()}`, async () => {
+    test(`errKey must be ${'error_from_deleteBookById'.toUpperCase()}`, async () => {
       expect(response._getData().errKey).toEqual(
-        'error_from_getBookById'.toUpperCase()
+        'error_from_deleteBookById'.toUpperCase()
       )
     })
   })
   describe('Case: Success', () => {
-    let currentBook = null
+    let bookId = null
     let response = null
 
     beforeAll(async (done) => {
       const currentBook = await Book.findOne()
-      const req = mockReq(currentBook)
+      bookId = currentBook._id
+      const req = mockReq(bookId)
       const res = httpMocks.createResponse()
-      response = await getBookById(req, res)
+      response = await deleteBookById(req, res)
       done()
     })
     test(`status must be 200`, async () => {
       expect(response.statusCode).toEqual(200)
+    })
+    test(`Should can not find book from bookId`, async () => {
+      const checkBook = await Book.findById(bookId)
+      expect(checkBook).toEqual(null)
     })
   })
   afterAll(async (done) => {
