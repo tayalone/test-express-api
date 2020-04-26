314f34bb6f12c1e40a02f0b9ccb904ca8772c316
estellar_boyz <tayalone@gmail.com>
2020-04-26 17:47:36 +0700

    :sparkles: [ADD] วางโตรง route get book by id

--- routes/v1/book.route/index.js
+++ routes/v1/book.route/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const { validate } = require('express-validation')
 
 const { getBooks } = require('../../../controllers/book')
 const { generateQueryParam } = require('../../../middlewares')
+const { getBookByIdValidate } = require('./validate')
 
 const router = express.Router()
 
 router.get('/', generateQueryParam, getBooks)
+
+router.get('/:bookId', validate(getBookByIdValidate), (req, res) => {
+  return res.send({ message: 'OK' })
+})
 
 module.exports = router
