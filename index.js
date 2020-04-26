9e66b6d45ac07268ca167ae09a19f573efc606cf
estellar_boyz <tayalone@gmail.com>
2020-04-26 16:13:42 +0700

    :white_check_mark: [add] test getbooks endpoints

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-Promise = require('bluebird')
-
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const compress = require('compression')
-const cors = require('cors')
-
-const { logs, port } = require('./variables')
+const app = require('./server')
 const mongoose = require('./mongoose')
-const routes = require('./routes/v1')
+const { port } = require('./variables')
 
 mongoose()
-
-const app = express()
-
-app.use(morgan(logs))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use(compress())
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors())
-
-// used Router version
-app.use('/v1', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 
-const bookRoutes = require('./book.route')
-
 const router = express.Router()
 
-router.get('/status', (req, res) => res.send('OK'))
-router.get('/', (req, res) => res.send('Hello World!'))
+const bookRoutes = require('./book.route')
 
 router.use('/books', bookRoutes)
 
